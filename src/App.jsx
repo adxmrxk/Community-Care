66eec54efe0f06c48cf085f48ea1213b15eb4bb7
Adam Rak <admarak239@gmail.com>
2025-08-18 10:29:49 -0400

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import React, { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='text-green-500'>Test</h1>
+    </div>
+  )
+}
+
+export default App
